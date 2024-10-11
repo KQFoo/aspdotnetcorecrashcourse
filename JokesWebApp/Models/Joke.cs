408bccf7a67ed599de6eae9582e41958050ff5cd
KQ <foongquan03@gmail.com>
2024-10-11 11:29:13 +0800

    added files

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        // prop + tab
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // ctor + tab
+        public Joke()
+        {
+            
+        }
+    }
+}
